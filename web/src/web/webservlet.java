86a0946531b3dd38ab6bdc73b90e40ee87dee404
usuario <usuario@simu02>
2020-02-17 11:57:53 +0100

    COOMIT

--- web/src/web/webservlet.java
+++ web/src/web/webservlet.java
@@ -2,26 +2,33 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.annotation.WebServlet;
 @SuppressWarnings("serial")
 @WebServlet("/")
+
+
 public class webservlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	private static final long serialVersionUID = 1L;
 
-        resp.setContentType("text/html");
-
-        PrintWriter out = resp.getWriter();
-        
-        out.println("<h1>Java Servlet Tutorial</h1>");
-        out.println("<p>Este es mi primer <b>Servlet</b>.</p>");
-        out.println("<p>Por: <a href=\"http://acodigo.blogspot.com\">Tutor de Programacion</a> </p>");
-    }
-}
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// Obtenemos un objeto Print Writer para enviar respuesta
+		res.setContentType("text/html");
+		PrintWriter pw = res.getWriter();
+		pw.println("<HTML><HEAD><TITLE>Leyendo parámetros</TITLE></HEAD>");
+		pw.println("<BODY BGCOLOR=\"#CCBBAA\">");
+		pw.println("<H2>Leyendo parámetros desde un formulario html</H2><P>");
+		pw.println("<UL>\n");
+		pw.println("Te llamas " + req.getParameter("NOM") + "<BR>");
+		pw.println("y tienes "  + req.getParameter("EDA") + " años<BR>");
+		pw.println("</BODY></HTML>");
+		pw.close();
+	}
+} 
